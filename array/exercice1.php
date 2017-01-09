8208807b106e73284a40fc5328ed7fde4675056f
Axel <lezinaxel@gmail.com>
2017-01-09 16:40:42 +0100

    Exercice 1 Initialisation du tableaux des mois

---
+++ array/exercice1.php
@@ -0,0 +1,16 @@
+<?php
+ $mois = [
+	mois1 => 'Janvier',
+	mois2 => 'Février',
+	mois3 => 'Mars',
+	mois4 => 'Avril',
+	mois5 => 'Mai',
+	mois6 => 'Juin',
+	mois7 => 'Juillet',
+	mois8 => 'Août',
+	mois9 => 'Septembre',
+	mois10 => 'Octbre',
+	mois11=> 'Novembre',
+	mois12 => 'Décembre'
+	];
+
