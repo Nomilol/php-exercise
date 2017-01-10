18d7ee7623fad78a4952069f1940dfd329254895
Axel <lezinaxel@gmail.com>
2017-01-10 10:48:02 +0100

    Exercice 9 Affichage des éléments du tableau

---
+++ array/exercice9.php
@@ -0,0 +1,12 @@
+<?php
+$departements = [
+	02 => 'Aisne',
+	59 => 'Nord',
+	60 => 'Oise',
+	62 => 'Pas-de-Calais',
+	80 => 'Somme'
+];
+
+foreach ($departements as $key => $value) {
+	echo $value;
+};
