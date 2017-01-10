3dba220857356a31c5d13105a8b3a4c570005c92
Axel <lezinaxel@gmail.com>
2017-01-10 10:43:40 +0100

    Exercice 8 Parcours du tableau avec une boucle et affichage des éléments

---
+++ array/exercice8.php
@@ -0,0 +1,5 @@
+<?php
+$mois = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+for ($i=0; $i <sizeof($mois) ; $i++) { 
+	echo $mois[$i];
+};
