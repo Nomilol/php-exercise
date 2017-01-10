d6ec4b393385f8ac227b572b1546fd2b8ce22028
Axel <lezinaxel@gmail.com>
2017-01-10 15:56:21 +0100

    Exercice 3 Affichage des données du formulaire

---
+++ formulaires/exercice3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="/user.php" method="GET">
+		<label for="name">Prénom</label>
+		<input type="text" id="name" name="name">
+		<label for="lastName">Nom</label>
+		<input type="text" id="lastName" name="lastName">
+		<button>Envoyer</button>
+	</form>
+</body>
+</html>
