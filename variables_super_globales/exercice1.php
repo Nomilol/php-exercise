2e08e12ca8e3ccce60abc81a4c31f559dfe2f948
Axel <lezinaxel@gmail.com>
2017-01-11 13:40:27 +0100

    Affichage des données à l'utilisateur (user agent , adresse ip et nom du serveur)

---
+++ variables_super_globales/exercice1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	print $_SERVER['HTTP_USER_AGENT'];
+	print $_SERVER['REMOTE_ADDR'];
+	print $_SERVER['SERVER_NAME'];
+	?>
+</body>
+</html>
